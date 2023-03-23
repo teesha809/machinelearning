796f0058eb6098817fc5066a00e41979a30f51e6
Sambhav Saxena <76242518+sambhavsaxena@users.noreply.github.com>
2023-03-23 14:08:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/python
 # -*- coding: utf-8 -*-
-# Copyright DCURST-ODC since 2012
+# Copyright DCURST-ODC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
